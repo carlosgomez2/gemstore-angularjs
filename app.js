b7e10945cf7a580c86c2099ebed6eebc9f1a3cb3
CARLOS GOMEZ <carlosgomez.deb@gmail.com>
2016-05-12 17:02:36 -0500

    inital commit

---
+++ app.js
@@ -0,0 +1,25 @@
+(function(){
+  
+  var app = angular.module('gemStore', [ ]);
+
+  app.controller('StoreController', function(){
+    this.product = gem;  
+  });
+  
+  var gems = [
+    {
+      name: 'Dodecaedro',
+      price: 2.95,
+      description: 'lorem ipsum ipsum lorem',
+      canPurchase: false,
+    },
+    {
+      name: 'Pentagonal Gem',
+      price: 5.95,
+      description: 'lorem ipsum lorem ipsum',
+      canPurchase: false,
+    }
+  ]
+  
+})();
+

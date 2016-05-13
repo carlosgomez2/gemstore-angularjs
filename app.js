ec66ddc4315f9586df04a5b0323fa90b78eef2db
CARLOS GOMEZ <carlosgomez.deb@gmail.com>
2016-05-13 12:36:34 -0500

    creating a controller for panel if tab is selected, cleaning app

--- app.js
+++ app.js
@@ -21,5 +21,18 @@
     }
   ];
   
+  app.controller("PanelController", function(){
+    this.tab = 1;
+    
+    this.selectTab = function(setTab){
+      this.tab = setTab;
+    };
+    
+    this.isSelected = function(checkTab){
+      return this.tab === checkTab;
+    };
+    
+  });
+  
 })();
 
